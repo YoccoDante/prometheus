05aa22d7dfa8ae51ec9780f54c2bba53caf7f5b5
Bryan Espinoza <bjespinozah@gmail.com>
2023-03-21 14:08:04 -0500

    Enable cors

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # main.py
 from flask import Flask
 from auth import bpAuth as auth
+from flask_cors import CORS
+
 
 
 app = Flask(__name__)
 app.register_blueprint(auth)
+CORS(app)
 
 
 if __name__ == "__main__":
